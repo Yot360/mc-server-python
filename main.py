d11a99734f2d6f5375696c48f50fd2b4ae7660b1
Yot360 <65982248+Yot360@users.noreply.github.com>
2021-05-20 20:31:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import urllib.request
 import progressbar
 import os
+import subprocess
 
+print("--------------------")
+print("Made by Yot, @Yot360")
+print("--------------------\n")
 
 #URLS FOR SERVER FILES
 paper_url = 'https://papermc.io/api/v1/paper/1.16.5/latest/download'
@@ -56,15 +60,100 @@ choice = int(input("(e.g 1 or 2) : "))
 
 if choice == 1:
     print("Downloading PaperMC latest server file: ")
-    urllib.request.urlretrieve(paper_url, path + '/paper.jar', show_progress)
-    print("Download Complete!")
+    urllib.request.urlretrieve(paper_url, path + '/server.jar', show_progress)
+    print("Download Complete!\n")
 elif choice == 2:
     print("Downloading Minecraft vanilla server latest file: ")
-    urllib.request.urlretrieve(vanilla_url, path + '/vanilla.jar', show_progress)
-    print("Download Complete!")
+    urllib.request.urlretrieve(vanilla_url, path + '/server.jar', show_progress)
+    print("Download Complete!\n")
 else:
     print("Please try again and choose a valid number.")
 
 
 #SERVER SETUP
-print("The server will now start setting up.")
+print("The server will now start and stop.\n")
+os.chdir(path)
+os.system('java -jar server.jar')
+eula = input("You need to accept the eula to launch your server. Accept it? [y/n] ")
+if eula == "y":
+    a_file = open("eula.txt", "r")
+    list_of_lines = a_file.readlines()
+    list_of_lines[2] = "eula=true\n"
+    a_file = open("eula.txt", "w")
+    a_file.writelines(list_of_lines)
+    a_file.close()
+    print("Eula accepted.\n")
+elif eula == "n":
+    print('Eula refused.\n')
+serv_prop_ask = input("Do you wish to change any settings in the system.properties file? [y/n] ")
+while True:
+    if serv_prop_ask == "y":
+        serv_prop_ask2 = int(input("\n1. Edit world seed\n2. Edit server description\n3. Edit max player value\n4. Edit view distance\n5. Server port\n6. Exit\n\n"))
+        if serv_prop_ask2 == 1:
+            serv_prop = open("server.properties", "r")
+            lines_prop = serv_prop.readlines()
+
+            seed = str(input("\nWhich seed would you like to set? "))
+            lines_prop[4] = "level-seed="+seed+"\n"
+            serv_prop = open("server.properties", "w")
+            serv_prop.writelines(lines_prop)
+            serv_prop.close()
+            print("\nSeed changed to "+seed+".")
+
+        elif serv_prop_ask2 == 2:
+            serv_prop = open("server.properties", "r")
+            lines_prop = serv_prop.readlines()
+            
+            name = str(input("\nWhich name would you like to set for your server? "))
+            lines_prop[10] = "motd="+name+"\n"
+            serv_prop = open("server.properties", "w")
+            serv_prop.writelines(lines_prop)
+            serv_prop.close()
+            print("\nServer name changed to "+name+".")
+        elif serv_prop_ask2 == 3:
+            serv_prop = open("server.properties", "r")
+            lines_prop = serv_prop.readlines()
+            
+            players = str(input("\nHow many players do you want to be able to join simultaneously? "))
+            lines_prop[18] = "max-players="+players+"\n"
+            serv_prop = open("server.properties", "w")
+            serv_prop.writelines(lines_prop)
+            serv_prop.close()
+            print("\nPlayer limit changed to "+players+".")
+        elif serv_prop_ask2 == 4:
+            serv_prop = open("server.properties", "r")
+            lines_prop = serv_prop.readlines()
+            
+            view = str(input("\nHow many chunks do you want the server to render? "))
+            lines_prop[23] = "view-distance="+view+"\n"
+            serv_prop = open("server.properties", "w")
+            serv_prop.writelines(lines_prop)
+            serv_prop.close()
+            print("\nRender distance changed to "+view+".")
+        elif serv_prop_ask2 == 5:
+            serv_prop = open("server.properties", "r")
+            lines_prop = serv_prop.readlines()
+            
+            port = str(input("\nWhat port do you want the server to run on? "))
+            lines_prop[27] = "server-port="+port+"\n"
+            serv_prop = open("server.properties", "w")
+            serv_prop.writelines(lines_prop)
+            serv_prop.close()
+            print("\nServer port changed to "+port+".")
+        elif serv_prop_ask2 == 6:
+            break
+start = input("\n Your server is now complete. Do you want to have a start.sh file to start it easily? [y/n] ")
+if start == "y":
+    ram = input("\nHow much ram do you want to give to the server? ")
+    start_sh = open("start.sh", "x")
+    start_sh.writelines('#!/bin/bash\njava -Xmx'+ram+'M -Xms'+ram+'M -jar server.jar nogui')
+    start_sh.close()
+    os.chmod('start.sh', 0o777)
+
+start_rn = input("\nDo you to start your server right now? [y/n] ")
+if start_rn == "y":
+    print("\nServer starting...")
+    os.system('./start.sh')
+if start_rn == "n":
+    print("Ok, if you want to launch your server simply run the start.sh file!")
+    print("Have fun!")
